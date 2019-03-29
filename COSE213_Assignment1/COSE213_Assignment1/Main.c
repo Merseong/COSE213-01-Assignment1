2d6d508e70b6f1a8c366c4485c0dd38419f51491
18신대성 <esc990720@korea.ac.kr>
2019-03-29 13:24:53 +0900

    main struct planning

--- COSE213_Assignment1/COSE213_Assignment1/Main.c
+++ COSE213_Assignment1/COSE213_Assignment1/Main.c
@@ -3,5 +3,23 @@
 int main()
 {
 	printf("hello world!");
+	/*
+	한줄의 입력을 쭉 받는다. 두개씩 분리해서 앞의 수는 계수, 뒤의 수는 차수
+
+	while (입력이 남아있는 동안)
+	{
+		PolyAdd(poly1, front, back);
+	}
+
+	두번째 줄의 입력을 받는다.
+
+	while (입력이 남아있는 동안)
+	{
+		PolyAdd(poly2, front, back);
+	}
+
+	PolyCombine(polysum, poly1, poly2);
+	printf(polysum);
+	*/
 	return 0;
 }
